69c196e572b153150789c4445881c8a217e8a5d4
Martin Hartman <hartmanx1@wp.pl>
2019-09-18 18:51:15 +0200

    change direct html

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,52 @@
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OptimizeJsPlugin = require('optimize-js-plugin');
 
-const plugins = [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'indexPlugin.html',
-      inject: 'body'
-  })];
-  
-  module.exports = (env) => {
-    const environment =  env || 'production';
+
+const plugins = [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
+
+module.exports = (env) => {
+    const environment = env || 'production';
     if (env === 'production') {
-      plugins.push(
-        new UglifyJSPlugin({
-          test: /\.js($|\?)/i
-      }),
-        new OptimizeJsPlugin({
-          sourceMap: false
-      })
-      )
+        plugins.push(
+            new UglifyJSPlugin({
+                test: /\.js($|\?)/i
+            }),
+            new OptimizeJsPlugin({
+                sourceMap: false
+            })
+        )
     }
     return {
-      mode: environment,
-      entry: './src/index.js',
-      output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.' + environment + '.bundle.js'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.js$/,
-            loader: 'babel-loader'
-          },
-          {
-            test: /\.css$/,
-            use: [
-              {loader: 'style-loader'},
-              {
-                loader: 'css-loader',
-                options: {
-                  modules: true
+        mode: environment,
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + environment + '.bundle.js'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true
+                    }
                 }
-              }
+                ]
+            }
             ]
-          }
-        ]
-      },
-      optimization: {
-        minimize: true
-      },
-      plugins
+        },
+        plugins: plugins
     }
-  };
+}; 
